d44449cef5827236cc066512b582aa322fee2502
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2021-12-03 21:12:04 +0100

    add fully working dapp

--- frontend-multisig-wallet/src/App.js
+++ frontend-multisig-wallet/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { getWeb3, getMultiSigWallet } from "./utils.js";
 import Header from "./Header";
+// TODO import newTrandfe component
+// TODO import transferList component
+import NewTransfer from "./NewTransfer.js";
+import TransferList from "./TransferList.js";
 
 function App() {
 	// when App component mounts we still haven't imported web3
@@ -11,15 +15,21 @@ function App() {
 	// Define quorum and approvers state (data from smart contract)
 	const [approvers, setApprovers] = useState([]);
 	const [quorum, setQuorum] = useState(undefined);
+	// todo transferst state
+	const [transfers, setTransfers] = useState([]);
 
 	useEffect(() => {
 		const init = async () => {
-			const web3 = getWeb3();
+			// todo await treba da e vaa funkcija aa kej a immplementiras so metamask
+			const web3 = await getWeb3();
 			const accounts = await web3.eth.getAccounts();
 			const wallet = await getMultiSigWallet(web3);
 			// ? read approvers and quorum from the smart contract
 			const approvers = await wallet.methods.getApprovers().call();
 			const quorum = await wallet.methods.quorum().call();
+			// todo load the transfers and update their state
+			const transfers = await wallet.methods.getTransfers().call();
+			setTransfers(transfers);
 			setWeb3(web3);
 			setAccounts(accounts);
 			setWallet(wallet);
@@ -28,6 +38,18 @@ function App() {
 		};
 		init();
 	}, []);
+
+	// TODO create the createTransfer function
+	// we use send because we want to create the transaction and
+	// we use
+	const createTransfer = (transfer) => {
+		wallet.methods.createTransfer(transfer.amount, transfer.to).send({ from: accounts[0] });
+	};
+
+	// todo approve transfer function and pass the approveTranfser function
+	const approveTransfer = (transferId) => {
+		wallet.methods.approveTransfer(transferId).send({ from: accounts[0] });
+	};
 
 	if (typeof web3 === "undefined" || typeof accounts === "undefined" || typeof wallet === "undefined" || approvers.length === 0 || typeof quorum === "undefined") {
 		return <div>Loading...</div>;
@@ -38,6 +60,8 @@ function App() {
 			{" "}
 			<p>Multi sig wallet app</p>
 			<Header approvers={approvers} quorum={quorum} />
+			<NewTransfer createTransfer={createTransfer} />
+			<TransferList transfers={transfers} approveTransfer={approveTransfer} />
 		</div>
 	);
 }
--- frontend-multisig-wallet/src/utils.js
+++ frontend-multisig-wallet/src/utils.js
@@ -4,14 +4,60 @@
 import Web3 from "web3";
 // import the contract artifact we got from previosly from deploying out smart contract
 import Wallet from "./contracts/MultiSigWallet.json";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 // a function that instantiates the web3 object
-const getWeb3 = () => {
-	// we are going to pass the url of the node that runs our development blockchain aka ganache
-	// http://127.0.0.1:9545/ in this case
-	// with this web3 object we have a connection to our local development blockchain
-	return new Web3("http://localhost:9545");
-};
+// TODO smeni a vaa funkcija da se integrira metammask treba takov web3 object ni treba
+const getWeb3 = () =>
+	new Promise(async (resolve, reject) => {
+		let provider = await detectEthereumProvider();
+
+		if (provider) {
+			await provider.request({ method: "eth_requestAccounts" });
+
+			try {
+				const web3 = new Web3(window.ethereum);
+
+				resolve(web3);
+			} catch (error) {
+				reject(error);
+			}
+		}
+		reject("Install Metamask");
+	});
+// const getWeb3 = () => {
+// 	return new Promise((resolve, reject) => {
+// 		// Wait for loading completion to avoid race conditions with web3 injection timing.
+// 		window.addEventListener("load", async () => {
+// 			// Modern dapp browsers...
+// 			if (window.ethereum) {
+// 				const web3 = new Web3(window.ethereum);
+// 				try {
+// 					// Request account access if needed
+// 					await window.ethereum.enable();
+// 					// Acccounts now exposed
+// 					resolve(web3);
+// 				} catch (error) {
+// 					reject(error);
+// 				}
+// 			}
+// 			// Legacy dapp browsers...
+// 			else if (window.web3) {
+// 				// Use Mist/MetaMask's provider.
+// 				const web3 = window.web3;
+// 				console.log("Injected web3 detected.");
+// 				resolve(web3);
+// 			}
+// 			// Fallback to localhost; use dev console port by default...
+// 			else {
+// 				const provider = new Web3.providers.HttpProvider("http://localhost:9545");
+// 				const web3 = new Web3(provider);
+// 				console.log("No web3 instance injected, using Local web3.");
+// 				resolve(web3);
+// 			}
+// 		});
+// 	});
+// };
 
 // create a contract instance - an object that is produced by web3
 // that allow you to directly interact with the smart contract to read and write data to the sc
