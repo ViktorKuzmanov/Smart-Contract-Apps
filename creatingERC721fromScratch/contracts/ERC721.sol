13cef0fc62de64f79e4a9922c1bf4e15570cc5bf
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2021-12-04 11:28:20 +0100

    add 2 more functions

--- creatingERC721fromScratch/contracts/ERC721.sol
+++ creatingERC721fromScratch/contracts/ERC721.sol
@@ -4,6 +4,7 @@ contract ERC721 {
 
     mapping(address=>uint256) internal _balances;
     mapping(uint256=>address) internal _owners;
+    mapping(address =>  mapping(address=>bool)) _operatorApprovals; 
 
     // Returns the number of NFTs assigned to an owner
     function balanceOf(address owner) public view returns(uint256) {
@@ -16,5 +17,18 @@ contract ERC721 {
         address owner = _owners[tokenId];
         require(owner!=address(0), "TokenID does not exist");
         return owner;
+    }
+
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+    // These are functions that define an operator
+    // Operator is any address that has the ability to manage NFTs for someone else
+    // Enables or disables an operator to manage all of msg.senders assets
+    function setApprovalForAll(address operator, bool approved) public {
+        _operatorApprovals[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+    // check if an address is an operator for another address
+    function isApprovedForAll(address owner, address operator) external view returns (bool) {
+        return _operatorApprovals[owner][operator];
     }
 }
