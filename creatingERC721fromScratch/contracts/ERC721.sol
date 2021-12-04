307ef1d33480c25545f406185f995ccce9b365dc
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2021-12-04 10:57:11 +0100

    add ownerOf function

--- creatingERC721fromScratch/contracts/ERC721.sol
+++ creatingERC721fromScratch/contracts/ERC721.sol
@@ -1,13 +1,20 @@
 pragma solidity ^0.8.2;
-
 contract ERC721 {
 
 
     mapping(address=>uint256) internal _balances;
-    
+    mapping(uint256=>address) internal _owners;
+
     // Returns the number of NFTs assigned to an owner
     function balanceOf(address owner) public view returns(uint256) {
         require(owner != address(0), "Address is zero");
         return _balances[owner];
+    }
+
+    // Find the owner of an NFT
+    function ownerOf(uint256 tokenId) public view returns(address) {
+        address owner = _owners[tokenId];
+        require(owner!=address(0), "TokenID does not exist");
+        return owner;
     }
 }
