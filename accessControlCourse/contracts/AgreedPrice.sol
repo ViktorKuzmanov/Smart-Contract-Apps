3135467ec1c9f10a129376228bcee0ede2475b3b
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2021-12-18 19:36:07 +0100

    import Ownable smart contract and inherit for it

--- accessControlCourse/contracts/AgreedPrice.sol
+++ accessControlCourse/contracts/AgreedPrice.sol
@@ -1,22 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.9;
+import "@openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract AgreedPrice {
+contract AgreedPrice is Ownable {
     uint256 public price;
-    address public owner;
     
     constructor(uint256 _price) {
         price = _price;
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can change the price");
-        _;
-    }
-
-    function changeOwner(address _newOwner) external onlyOwner {
-        owner = _newOwner;
     }
 
     function updatePrice(uint256 _price) external onlyOwner {
