c15ef748f1cd0d8d14194fa0bb12a86d895747cd
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2021-12-18 13:46:28 +0100

    rename file and add other tests

---
+++ accessControlCourse/test/access-control-test.js
@@ -0,0 +1,37 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Access Control", function () {
+
+  let deployer, attacker;
+
+  this.beforeEach(async function() {
+    [deployer, attacker] = await ethers.getSigners();
+
+    const AgreedPrice = await ethers.getContractFactory("AgreedPrice", deployer);
+    this.agreedPrice = await AgreedPrice.deploy(100);
+  })
+
+  describe("AgreedPrice", ()=>{
+    it("should set price at deployment", async function() {
+      expect(await this.agreedPrice.price()).to.eq(100);
+    })
+  })
+
+  // this test should fail:
+  // it("should be possible for anyone to change price", async function() {
+  //   await this.agreedPrice.connect(attacker).updatePrice(1000);
+  //   expect(await this.agreedPrice.price()).to.eq(1000);
+  // })
+
+  it("should set the deployer account as the owner", async function() {
+    expect(await this.agreedPrice.owner()).to.eq(deployer.address);
+  })
+
+  it("should NOT be possible for other then the owner to change the price", async function(){
+    // the owner is trying to set the price
+    await this.agreedPrice.updatePrice(1000);
+    expect(await this.agreedPrice.price()).to.eq(1000);
+  })
+  
+});
