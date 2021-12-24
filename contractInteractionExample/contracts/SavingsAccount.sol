78dd37a62aa7243eb5f26b8342bc73725a16b652
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2021-12-24 12:30:52 +0100

    replace transfer with sendValue

--- contractInteractionExample/contracts/SavingsAccount.sol
+++ contractInteractionExample/contracts/SavingsAccount.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/utils/Address.sol";
+
 contract SavingsAccount {
+    using Address for address payable;
     mapping(address=>uint256) public balanceOf;
 
     function deposit() external payable {
@@ -11,6 +14,6 @@ contract SavingsAccount {
     function withdraw() external payable {
         uint256 amountDeposited = balanceOf[msg.sender];
         balanceOf[msg.sender] = 0;
-        payable(msg.sender).transfer(amountDeposited);
+        payable(msg.sender).sendValue(amountDeposited);
     }
 }
