39ab1bb27395ea2225de6153e64ea3c058a902f2
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2021-12-24 13:13:41 +0100

    test if SavingsAccount works as expected

--- contractInteractionExample/test/sample-test.js
+++ contractInteractionExample/test/sample-test.js
@@ -46,3 +46,38 @@ describe("Testing SavingsAccount contract", function () {
     })
   })
 });
+
+
+describe("Testing SavingsAccountV2 and InvestorV2 contracts", function () {
+  let deployer, user, attacker;
+
+  beforeEach(async function() {
+    [deployer, user] = await ethers.getSigners();
+    
+    const SavingsAccount = await ethers.getContractFactory("SavingsAccountV2", deployer);
+    this.savingsAccountV2 = await SavingsAccount.deploy();
+
+    await this.savingsAccountV2.deposit({value: ethers.utils.parseEther("100")});
+    await this.savingsAccountV2.connect(user).deposit({value: ethers.utils.parseEther("50")});
+
+    const Investor = await ethers.getContractFactory("InvestorV2", deployer);
+    this.investor = await Investor.deploy(this.savingsAccountV2.address);
+  })
+
+  describe("SavingsAccountV2 test", function() {
+    it("Funds should already be deposited", async function() {
+      expect(await this.savingsAccountV2.balanceOf(deployer.address)).to.eq(ethers.utils.parseEther("100"));
+      expect(await this.savingsAccountV2.balanceOf(user.address)).to.eq(ethers.utils.parseEther("50"));
+    })
+    
+    it("Should accept withdraws", async function() {
+      this.savingsAccountV2.withdraw();
+
+      const deployerBalance = await this.savingsAccountV2.balanceOf(deployer.address);
+      const userBalance = await this.savingsAccountV2.balanceOf(user.address);
+
+      expect(deployerBalance).to.eq(0);
+      expect(userBalance).to.eq(ethers.utils.parseEther("50"));
+    })
+  })
+});
