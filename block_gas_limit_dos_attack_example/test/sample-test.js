61e1a7401668e1485270deae1a39a5d94468d923
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2021-12-28 13:19:15 +0100

    add AuctionV2 and tests for it

--- block_gas_limit_dos_attack_example/test/sample-test.js
+++ block_gas_limit_dos_attack_example/test/sample-test.js
@@ -6,14 +6,14 @@ describe("Dos attack", function () {
   beforeEach(async function() {
     [deployer, attacker, user] = await ethers.getSigners();
 
-    const Auction = await ethers.getContractFactory("Auction", deployer);
-    this.auction = await Auction.deploy();
+    const AuctionV2 = await ethers.getContractFactory("AuctionV2", deployer);
+    this.auctionV2 = await AuctionV2.deploy();
 
-    this.auction.bid({value: 100});
+    this.auctionV2.bid({ value: 100 });
   })
 
 
-  describe("Auction", function() {
+  describe.skip("Auction", function() {
     describe("if bid is lower than higherstBid", function() {
       it("Should NOT accept bids lower than current", async function () {
         await expect(this.auction.connect(user).bid({value: 50})).to.be.revertedWith("Not big enough bid");
@@ -35,24 +35,56 @@ describe("Dos attack", function () {
         expect(amount).to.eq(100);
       });
     })
+
+    describe("When calling refundAll", function() {
+      it("Should refund the bidders that didn't win", async function () {
+        await this.auction.connect(user).bid({ value: 150 });
+          await this.auction.bid({ value: 200 });
+  
+          const userBalanceBefore = await ethers.provider.getBalance(user.address);
+          await this.auction.refundAll();
+          const userBalanceAfter = await ethers.provider.getBalance(user.address);
+  
+          expect(userBalanceAfter).to.eq(userBalanceBefore.add(150));
+      });
+      it("Should revert if the amount of computations hits the block gas limit", async function () {
+        for (let i = 0; i < 1500; i++) {
+          await this.auction.connect(attacker).bid({ value: 150 + i });
+        }
+        await this.auction.refundAll();
+      });
+    })
   })
 
-  describe("When calling refundAll", function() {
-    it("Should refund the bidders that didn't win", async function () {
-      await this.auction.connect(user).bid({ value: 150 });
-        await this.auction.bid({ value: 200 });
+  describe("AuctionV2", function () {
+    describe("Pull over push solution", function () {
+      it("A user should be able to be refunded for a small number of bids", async function () {
+        await this.auctionV2.connect(user).bid({ value: ethers.utils.parseEther("1") });
+
+        await this.auctionV2.bid({ value: ethers.utils.parseEther("2") });
 
         const userBalanceBefore = await ethers.provider.getBalance(user.address);
-        await this.auction.refundAll();
+
+        await this.auctionV2.connect(user).withdrawRefund();
+
         const userBalanceAfter = await ethers.provider.getBalance(user.address);
 
-        expect(userBalanceAfter).to.eq(userBalanceBefore.add(150));
+        expect(userBalanceAfter).to.be.gt(userBalanceBefore);
+      });
+      it("A user should be able to be refunded for a very large number of bids", async function () {
+        for (let i = 0; i < 1500; i++) {
+          await this.auctionV2.connect(user).bid({ value: ethers.utils.parseEther("0.0001") + i });
+        }
+
+        const userBalanceBefore = await ethers.provider.getBalance(user.address);
+
+        await this.auctionV2.connect(user).withdrawRefund();
+
+        const userBalanceAfter = await ethers.provider.getBalance(user.address);
+
+        expect(userBalanceAfter).to.be.gt(userBalanceBefore);
+      });
     });
-    it("Should revert if the amount of computations hits the block gas limit", async function () {
-      for (let i = 0; i < 1500; i++) {
-        await this.auction.connect(attacker).bid({ value: 150 + i });
-      }
-      await this.auction.refundAll();
-    });
-  })
+  });
+    
 });
